544ff62faa263f46501bc4251babf38683eee9bf
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-21 11:36:50 -0300

    criacao do models e admin do app categoria e suas migracoes

--- comentarios/admin.py
+++ comentarios/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Comentario
 
-# Register your models here.
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_comentario', 'email_comentario', 'post_comentario',
+                    'data_comentario', 'publicacao_comentario',)
+    list_editable = ('publicacao_comentario',)
+    list_display_links = ('id', 'nome_comentario', 'email_comentario',)
+
+
+admin.site.register(Comentario, ComentarioAdmin)
