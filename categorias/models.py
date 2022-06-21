4bdee184b56036cc99e57d78749aefd8f2c9c3ce
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-21 11:53:59 -0300

    editando nome dos titulos da area administrativa

--- categorias/models.py
+++ categorias/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    nome_cat = models.CharField(max_length=50)
+    nome_cat = models.CharField(max_length=50, verbose_name='Nome da Categoria')
+
+    def __str__(self):
+        return self.nome_cat
--- comentarios/models.py
+++ comentarios/models.py
@@ -5,10 +5,13 @@ from django.utils import timezone
 
 
 class Comentario(models.Model):
-    nome_comentario = models.CharField(max_length=150)
-    email_comentario = models.EmailField()
-    comentario = models.TextField()
-    post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE)
+    nome_comentario = models.CharField(max_length=150, verbose_name='Nome')
+    email_comentario = models.EmailField(verbose_name='Email')
+    comentario = models.TextField(verbose_name='Comentário')
+    post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post do comentário')
     usuario_comentario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    data_comentario = models.DateTimeField(default=timezone.now)
-    publicacao_comentario = models.BooleanField(default=False)
+    data_comentario = models.DateTimeField(default=timezone.now, verbose_name='Data')
+    publicacao_comentario = models.BooleanField(default=False, verbose_name='Publicar')
+
+    def __str__(self):
+        return self.nome_comentario
