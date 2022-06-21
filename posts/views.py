f859ccd88061958dfaaf0e8368e895961f77e87c
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-21 19:12:18 -0300

    criando o template para o index.html com a exibicao dos posts com sua imagem, excerto, data, qtd comentarios e categoria

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+from posts.models import Post
 
 
 class PostIndex(ListView):
-    pass
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 6
+    context_object_name = 'posts'
 
 
 class PostBusca(PostIndex):
