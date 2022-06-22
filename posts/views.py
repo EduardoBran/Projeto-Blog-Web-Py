fcc72ecdac0f1b7b55210c80e62018fdac3b3e8a
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-22 15:08:56 -0300

    alterando exibicao dos post para somente os que estao marcados

--- posts/views.py
+++ posts/views.py
@@ -13,7 +13,7 @@ class PostIndex(ListView):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        qs = qs.order_by('-id')
+        qs = qs.order_by('-id').filter(publicado_post=True)
         qs = qs.annotate(
             numero_comentarios=Count(
                 Case(
