6b783fa9bb394f83e8cadd9223d16af8f8d6a224
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-22 10:47:28 -0300

    exibicao da ordem dos posts, correcao ao exibir do numero de comentarios

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
@@ -9,6 +10,18 @@ class PostIndex(ListView):
     template_name = 'posts/index.html'
     paginate_by = 6
     context_object_name = 'posts'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id')
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicacao_comentario=True, then=1)
+                )
+            )
+        )
+        return qs
 
 
 class PostBusca(PostIndex):
