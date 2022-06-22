b35dc3f7a58ac707059606f1c9e66c3d8c26b042
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-22 20:13:57 -0300

    Implementando busca ao clicar nas categorias e campo busca

--- posts/views.py
+++ posts/views.py
@@ -35,9 +35,42 @@ class PostIndex(ListView):
 class PostBusca(PostIndex):
     template_name = 'posts/post_busca.html'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        # filtrando a busca
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo) |
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+
+
+        return qs
+
 
 class PostCategoria(PostIndex):
     template_name = 'posts/post_categoria.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return qs
+
+        # mudando a consulta
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+
+        return qs
 
 
 class PostDetalhes(UpdateView):
